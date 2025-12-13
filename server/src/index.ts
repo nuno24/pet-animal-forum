1531b7c65198aee3859b44c199aa7efa72ed3d47
nuno24 <nuno.m1233@gmail.com>
2025-12-13 15:27:27 +0000

    Working prisma setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
+import { prisma } from "./lib/prisma"
 
 const app = express();
 const PORT = 3000;
+
+prisma.$connect()
+  .then(() => console.log("Connected to DB"))
+  .catch((err:any) => console.error("DB connection failed:", err))
 
 app.get("/", (req: Request, res: Response) => {
   console.log("GET / HITTED");
