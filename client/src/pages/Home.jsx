2e66845928cea4c0303723a318367f5860ca2453
nuno24 <nuno.m1233@gmail.com>
2025-12-18 17:45:47 +0000

    Logout redirects to login page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import { useAuth } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
   const {user, logout} = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout
+    navigate("/login")
+  }
 
   return(
     <div>
@@ -9,7 +16,7 @@ export default function Home() {
       <h2>User: {user.username}</h2>
       <h2>Email: {user.email}</h2>
       <h2>Role: {user.role}</h2>
-      <button onClick={logout}>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
